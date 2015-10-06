d724539eb81f1c868523a487769883ba577f5366
n-blaski <webstudent@pc16287.rvc.cc.il.us>
2015-10-05 19:14:23 -0500

    Initial Commit

---
+++ Top 10 Delicious FoodsTests/Top_10_Delicious_FoodsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Top_10_Delicious_FoodsTests.swift
+//  Top 10 Delicious FoodsTests
+//
+//  Created by webstudent on 10/5/15.
+//  Copyright © 2015 RVC. All rights reserved.
+//
+
+import XCTest
+@testable import Top_10_Delicious_Foods
+
+class Top_10_Delicious_FoodsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
